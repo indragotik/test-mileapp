06893ba7d8aa5551b0443e764969cf461bd975f5
indrapermana <indra.starts.permana@gamil.com>
2025-10-25 19:25:17 +0700

    update

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -5,14 +5,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::prefix('api')->middleware('api')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+
+    Route::middleware('mock.auth')->group(function () {
+        // resource tasks (GET/POST/PUT/DELETE) => /api/tasks
+        Route::apiResource('tasks', TaskController::class);
+    });
 });
 
-Route::middleware('mock.auth')->group(function () {
-    Route::apiResource('tasks', TaskController::class);
-});
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::prefix('api')->middleware('api')->group(function () {
+//     Route::post('/login', [AuthController::class, 'login']);
+// });
+
+// Route::middleware('mock.auth')->group(function () {
+//     Route::apiResource('tasks', TaskController::class);
+// });
