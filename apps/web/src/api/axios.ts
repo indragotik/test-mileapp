272530c47040899d422761bd0e7e8b8d3733ce0a
indrapermana <indra.starts.permana@gamil.com>
2025-10-25 15:38:39 +0700

    update env web

--- apps/web/src/api/axios.ts
+++ apps/web/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://test-mileapp-api-1.vercel.app/api',
 })
 
 api.interceptors.request.use((config) => {
