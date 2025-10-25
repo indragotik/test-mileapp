843aa76cdcddecf2789149f0ad1f39e0c2ed637d
indrapermana <indra.starts.permana@gamil.com>
2025-10-25 19:17:06 +0700

    update

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -9,7 +9,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('api')->middleware('api')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::middleware('mock.auth')->group(function () {
     Route::apiResource('tasks', TaskController::class);
