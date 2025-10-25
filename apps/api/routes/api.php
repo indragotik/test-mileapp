deb78ee1b57af4fdad25e1346be2e5baa5fe3a10
indrapermana <indra.starts.permana@gamil.com>
2025-10-25 20:42:22 +0700

    update

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 
+Route::get('/', function () {
+    return require __DIR__ . "/../public/index.php";
+});
+
 Route::middleware('api')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
 });
